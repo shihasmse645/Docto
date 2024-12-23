89e7492c7b438e235b9cb8ece0441125bd6cdc96
Muhammed Shihas <muhammedshihase@gmail.com>
2024-12-23 07:11:38 -0600

    splashscreen implemented

---
+++ android/app/src/main/kotlin/com/example/ghotest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ghotest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Views/SplashScreen.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:ghotest/Views/Home.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Set a timer to navigate to HomeScreen
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Image.asset(
+          'assets/images/logos.png',
+          height: 200,
+          width: 200,
+        ),
+      ),
+    );
+  }
+}
